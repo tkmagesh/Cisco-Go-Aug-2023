0624aa10841eea4ca155b2688705e840e7c92c8f
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-23 14:01:09 +0530

    09-assignment

---
+++ 13-concurrency/09-assignment.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	data := [][]int{
+		{10, 20},
+		{30, 20},
+		{20, 40},
+		{30, 50},
+		{50, 60},
+		{70, 50},
+		{90, 80},
+		{80, 70},
+		{80, 60},
+		{30, 60},
+	}
+
+	/*
+		Sum all the numbers in "data" concurrently
+		   Note : spin one goroutine for each pair of numbers
+	*/
+}
+
+func add(x, y int) {
+
+}
